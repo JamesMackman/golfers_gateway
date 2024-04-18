8712eee750f775e1d039e20deaf8fe33a80bf12e
James Mackman <jmackman@live.co.uk>
2024-04-18 14:01:59 +0000

    Add a golfers profile model and a golf club model

--- golfers_gateway/admin.py
+++ golfers_gateway/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import GolfersProfile, GolfClub
 
 # Register your models here.
+@admin.register(GolfersProfile)
+class GolfersProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'handicap')
+    search_fields = ('user__username', 'bio')
+
+@admin.register(GolfClub)
+class GolfClubAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location_latitude', 'location_longitude', 'rating')
+    search_fields = ('name', 'description')
--- golfers_gateway/models.py
+++ golfers_gateway/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class GolfersProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+    handicap = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    favorite_courses = models.ManyToManyField('GolfClub', blank=True)
+
+class GolfClub(models.Model):
+    name = models.CharField(max_length=100)
+    location_latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    location_longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    description = models.TextField()
+    image = models.ImageField(upload_to='golf_club_images/')
+    contact_information = models.CharField(max_length=200)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
